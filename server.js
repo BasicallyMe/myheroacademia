409718157a8d0741f4f2c7642073a7ab080c60ed
Abajit Dey <abajitdeyag10118@gmail.com>
2022-10-24 08:53:55 +0530

    Changes to dependency
    Added nodemon as dev dependency, changes to start scripts for deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 
 app.get('/api/welcome', (req, res) => {
