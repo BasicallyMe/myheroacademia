f86b30455c00c872fe45642f527b20b7c3fc885e
Abajit Dey <abajitdeyag10118@gmail.com>
2022-10-24 08:50:09 +0530

    First commit
    First commit to the repo, basic folder structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/api/welcome', (req, res) => {
+    res.json({ message: "Welcome to MHA"});
+});
+
+
+app.listen(PORT);
